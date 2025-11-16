fc942189b43c8db2b4fbc3987fc099c0b0c1ef4e
Aymen tigui <aymentigui@gmail.com>
2025-11-16 15:43:58 +0100

    ok2

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config:any) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      path: false,
-    };
-    return config;
-  },
-  // Pour éviter les problèmes avec les librairies de scan
-  experimental: {
-    esmExternals: 'loose'
-  }
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
